716d555166874be7bbf20e74d42760d4ae162162
CosineAA <100404990+CosineAA@users.noreply.github.com>
2023-07-04 20:21:18 +0900

    Update PacketShowTool.kt

--- PacketShowTool.kt
+++ PacketShowTool.kt
@@ -1,27 +1,35 @@
 class PacketShowTool : JavaPlugin() {
 
+    // 1.19.4 기준임
     override fun onEnable() {
-        // 1.19.4 기준임
+        server.onlinePlayers.forEach {
+            val packetShowTool = PacketShowTool()
+            it.playerConnection.getNetworkManager().addHandler(packetShowTool)
+        }
+
         server.pluginManager.registerEvents(object : Listener {
 
             @EventHandler
             fun onJoin(event: PlayerJoinEvent) {
                 val player = event.player
-                val entityPlayer = (player as CraftPlayer).entityPlayer.b
-
+                val playerConnection = (player as CraftPlayer).entityPlayer.b
                 val packetShowTool = PacketShowTool()
+                playerConnection.getNetworkManager().addHandler(packetShowTool)
+            }
 
-                val networkManagerField = PlayerConnection::class.java.getDeclaredField("h")
-                networkManagerField.isAccessible = true
-                val networkManager = networkManagerField.get(entityPlayer) as NetworkManager
-
-                networkManager.m.pipeline().addBefore("packet_handler", "z", packetShowTool)
+            @EventHandler
+            fun onQuit(event: PlayerQuitEvent) {
+                val player = event.player
+                val playerConnection = (player as CraftPlayer).entityPlayer.b
+                playerConnection.getNetworkManager().removeHandler()
             }
         }, this)
     }
 
     override fun onDisable() {
-        
+        server.onlinePlayers.forEach {
+            it.playerConnection.getNetworkManager().removeHandler()
+        }
     }
 }
 
@@ -36,4 +44,18 @@ class PacketShowTool : ChannelDuplexHandler() {
         println("Write: ${msg::class.simpleName}")
         super.write(ctx, msg, promise)
     }
+}
+
+private fun PlayerConnection.getNetworkManager(): NetworkManager {
+    val networkManagerField = PlayerConnection::class.java.getDeclaredField("h")
+    networkManagerField.isAccessible = true
+    return networkManagerField.get(this) as NetworkManager
+}
+
+private fun NetworkManager.addHandler(channelHandler: ChannelHandler) {
+    m.pipeline().addBefore("packet_handler", "trade_handler", channelHandler)
+}
+
+private fun NetworkManager.removeHandler() {
+    m.pipeline().remove("trade_handler")
 }
